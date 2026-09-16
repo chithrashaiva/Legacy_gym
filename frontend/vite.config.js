f0c50641b0e4c1777d09fb71c642e3a108841672
chithrashaiva <chithrashaiva29@gmail.com>
2026-09-16 22:45:34 +0530

    Update allowedHosts to ['.onrender.com'] in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: true
-  },
-  server: {
-    allowedHosts: true
+    allowedHosts: ['.onrender.com']
   }
 })
